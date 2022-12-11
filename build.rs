8caddab14af13e4d66f16370e064b7510a45c397
RinLovesYou <xxklintan123xx@gmail.com>
2022-12-11 18:30:37 +0100

    hm

--- build.rs
+++ build.rs
@@ -30,7 +30,9 @@ fn link_dobby() {
     println!("cargo:warning=lib_path={}", lib_path.display());
     println!("cargo:rustc-link-search=native={}", lib_path.display());
     println!("cargo:rustc-link-lib=static=dobby");
-    println!("cargo:rustc-link-lib=dylib=stdc++");
+    #[cfg(not(target_os = "windows"))] {
+        println!("cargo:rustc-link-lib=dylib=stdc++");
+    }
 }
 
 fn main() {
